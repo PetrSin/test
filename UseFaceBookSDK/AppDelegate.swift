d8b02c1997c777ac48cb85331fac10f8c33570b6
Kirill Romanenko <kirrom@bk.ru>
2023-11-08 23:48:34 +0300

    refactoring

--- UseFaceBookSDK/AppDelegate.swift
+++ UseFaceBookSDK/AppDelegate.swift
@@ -12,14 +12,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+    func application(_ application: UIApplication, 
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.backgroundColor = .white
         let mainVC = ViewController()
         window?.rootViewController = mainVC
         window?.makeKeyAndVisible()
-        print("asdgasg")
         return true
     }
 }
--- UseFaceBookSDK/ViewController.swift
+++ UseFaceBookSDK/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     let myButton: UIButton = {
         var view = UIButton(type: .system)
         view.setTitle("hello", for: .normal)
@@ -16,43 +15,47 @@ class ViewController: UIViewController {
         view.titleLabel?.font = .systemFont(ofSize: 15, weight: .bold)
         view.layer.borderWidth = 3.0
         view.layer.borderColor = UIColor.black.cgColor
+        view.layer.cornerRadius = 30
         return view
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        createGradientsBackground(firstColor: .green, secondColor: .red, gradientDirection: .vertically, forView: view)
+        view.createAndApplyGradient(colors: [.red, .green], angle: 90)
         myButton.frame = CGRect(x: 0, y: 0, width: Int(view.bounds.width) - 64, height: 60)
         myButton.center = view.center
-        createGradientsBackground(firstColor: .blue, secondColor: .purple, gradientDirection: .horizontally, forView: myButton)
+        myButton.createAndApplyGradient(colors: [.blue, .purple])
+        myButton.clipsToBounds = true
         view.addSubview(myButton)
-        
     }
-
-
-}
-enum Direction{
-        case horizontally
-        case vertically
-    }
-
-extension ViewController {
     
-    func createGradientsBackground(firstColor: UIColor, secondColor: UIColor, gradientDirection: Direction, forView: AnyObject) {
-        
-        let gradientsLayer =  CAGradientLayer()
-        gradientsLayer.frame = forView.bounds
-        
-        switch gradientDirection{
-        case .horizontally:
-            gradientsLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
-            gradientsLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
-        case .vertically:
-            gradientsLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
-            gradientsLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
-        }
-        
-        gradientsLayer.colors = [firstColor.cgColor, secondColor.cgColor]
-        forView.layer.addSublayer(gradientsLayer)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        myButton.setNeedsDisplay()
+    }
+}
+
+extension UIView {
+    @discardableResult
+    func createAndApplyGradient(colors: [UIColor],
+                                angle: Double = 0) -> CAGradientLayer {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.apply(angle: angle)
+        gradientLayer.colors = colors.map { $0.cgColor }
+        layer.insertSublayer(gradientLayer, at: 0)
+        return gradientLayer
+    }
+}
+
+extension CAGradientLayer {
+    func apply(angle: Double) {
+        let x: Double = angle / 360.0
+        let a = pow(sinf(Float(2 * Double.pi * ((x + 0.75) / 2))), 2)
+        let b = pow(sinf(Float(2 * Double.pi * ((x + 0.0) / 2))), 2)
+        let c = pow(sinf(Float(2 * Double.pi * ((x + 0.25) / 2))), 2)
+        let d = pow(sinf(Float(2 * Double.pi * ((x + 0.5) / 2))), 2)
+        startPoint = CGPoint(x: CGFloat(a), y: CGFloat(b))
+        endPoint = CGPoint(x: CGFloat(c), y: CGFloat(d))
     }
 }
